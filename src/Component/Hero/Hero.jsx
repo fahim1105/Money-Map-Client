be3d5407320fa8cc01acecee9bd0ebe0f7558be3
Asif Al Fattha Fahim <asifalfatthafahim12@gmai.com>
2025-11-13 17:49:50 +0600

    add overview conditonal

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { use } from "react";
 import Banner from "./Banner/Banner";
 import StaticSections from "./StaticSections/StaticSections";
 import Overview from "./Overview/Overview";
+import { AuthContext } from "../../Provider/AuthContext/AuthContext";
 
 const Hero = () => {
+    const { user } = use(AuthContext)
     return (
         <main className="w-full overflow-hidden">
             <Banner />
             <section >
                 <StaticSections />
             </section>
-            <Overview></Overview>
+            {
+                user &&
+                <Overview></Overview>
+            }
         </main>
     );
 };
